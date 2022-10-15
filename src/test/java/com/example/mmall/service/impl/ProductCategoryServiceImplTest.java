6b9dba902596f7f7708f40460bfede5e9b3afc08
29741 <2974150895@qq.com>
2022-10-15 20:08:16 +0800

    Mall商城

---
+++ src/test/java/com/example/mmall/service/impl/ProductCategoryServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.example.mmall.service.impl;
+
+import com.example.mmall.service.ProductCategoryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductCategoryServiceImplTest {
+
+    @Autowired
+    private ProductCategoryService service;
+
+    @Test
+    void getAllProductCategoryVO() {
+        service.getAllProductCategoryVO();
+    }
+}
